4053aef5beec33ec087e4825b89a8f1610dcb4fb
Tharun <tharungowda30115@gmail.com>
2025-01-28 23:37:08 +0530

    minor updates

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
-
 let allTodos = getTodos();
 updateTodoList();
-
 todoForm.addEventListener('submit', function(e){
     e.preventDefault();
     addTodo();
 })
-
 function addTodo(){
     const todoText = todoInput.value.trim();
     if(todoText.length > 0){
